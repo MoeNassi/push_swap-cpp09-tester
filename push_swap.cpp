ea7de6eb71290460378124e49b138284cd1074d6
Mohammed Nassi <mnassi@e2r7p6.1337.ma>
2024-02-17 15:43:04 +0100

    all good

--- push_swap.cpp
+++ push_swap.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
+#include <limits.h>
 
 #define AllowChars "\"-"
+#define Disallowed  "abcdefghijklmnopqrstuvwxyz"
 
-int main() {
+void	Allowed() {
+	int	quote = 0;
 	srand(time(0));
 	std::string argument;
 	for (int i = 0; i < 30; i++) {
-		int random = rand() % 150;
-		if (random % 2 == 0)
+		int random = rand() % 10000;
+		if (random % 1 == 0)
 			argument += '\"';
 		argument += std::to_string(random) + " ";
 	}
+	for (int i = 0; i < argument.length(); i++)
+		if (argument[i] == '\"')
+			quote++;
+	if (quote % 2)
+		argument += '\"';
 	std::cout << argument << std::endl;
+}
+
+void	Forbidden() {
+	srand(time(0));
+	std::string argument;
+	for (int i = 0; i < 30; i++) {
+		int random = rand() % SIZE_T_MAX;
+		if (random % 2 == 0)
+			argument += '\"';
+		argument += std::to_string(random);
+	}
+	std::cout << argument << std::endl;
+}
+
+int main(int ac, char **av) {
+	(void)ac;
+	if (ac != 2)
+		return 0;
+	if (av[1][0] == '1')
+		Allowed();
+	else
+		Forbidden();
 }
--- tester.sh
+++ tester.sh
@@ -16,100 +16,146 @@ Purple='\033[0;35m'
 Cyan='\033[0;36m'  
 White='\033[0;37m' 
 
-echo -e "${White}Parsing Tester -> (still gonna add more tests)${Color_Off}"
+echo -e "${White}Parsing Tester -> (all tests needs to be passed successfuly)${Color_Off}"
 
-while [ $counter -le 50 ] ;
-	do
-		args=$(c++ push_swap.cpp)
-		status=$(./push_swap $args)
-		if [ "$status" = "Error" ] ; then
-			echo -e "${Red}Error in parsing! ${Color_Off}"
-			exit 0
+if [ -x "$executable" ] ; then
+	while [ $counter -le 50 ] ;
+		do
+			args=$(c++ push_swap.cpp && ./a.out 1)
+			# sleep 1
+			status=$(./$executable $args)
+			if [ "$status" = "Error" ] ; then
+				echo -e "${Green}test passed Successfuly ${Color_Off}" $args
+			else
+				echo -e "${Red}Error in parsing! ${Color_Off}" $args
+				echo -e "-> $args\n" >> failed_test.txt
+			fi
+			((counter++))
+		done
+
+	echo -e "${White}Parsing Tester -> (nothing should work)${Color_Off}"
+
+	counter=1
+
+	while [ $counter -le 50 ] ;
+		do
+			args=$(c++ push_swap.cpp && ./a.out 0)
+			status=$(./push_swap $args)
+			if [ "$status" = "Error" ] ; then
+				echo -e "${Green}Passed ! ${Color_Off}"$args
+			else
+				echo -e "${Red}This should not work ${Color_Off} - > "$args
+				echo -e "$args \n" >> failed_test.txt
+			fi
+			((counter++))
+		done
+
+	while true ; do
+		echo -e "options${Blue} : 3 5 100 500"
+		echo -e "${Cyan}Whats your desire number to test : ${Color_Off}"
+		read response
+
+		if [ $response -eq 500 ] ; then
+			if [ -x "$executable" ] ; then
+				loop=0
+				while [ $loop -le 200 ];
+				do
+					input=$(jot -r 500 -10000000 10000000)
+					output=$(./$executable $input | wc -l)
+					out=$(./$executable $input)
+					if [[ $out =~ ^[0-9]+$ ]] ; then
+						echo -e "${Red}Error: ${Color_Off}push_swap returned a malformed return"
+					fi
+					if [ $out = "Error" ] ; then
+						echo -e "${Purple}Your Push Swap returned ${Color_Off}${Red}Error${Color_Off}"
+					elif [ $output -lt 5501 ] ; then
+						echo -e "${Green}success -> $output ${Color_Off}"
+					else
+						echo -e "${Red}Error in instructions the number is : ${Color_Off}" $output
+					fi
+					((loop++))
+				done
+			else
+				echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
+			fi
+
+		elif [ $response -eq 100 ] ; then
+			if [ -x "$executable" ] ; then
+				loop=0
+				while [ $loop -le 200 ]; 
+				do
+					input=$(jot -r 100 -10000000 10000000)
+					output=$(./$executable $input | wc -l)
+					out=$(./$executable $input)
+					if [[ $out =~ ^[0-9]+$ ]]; then
+						echo -e "${Red}Error: ${Color_Off}push_swap returned a malformed return"
+					fi
+					if [[ $out = "Error" ]] ; then
+						echo -e "${Purple}Your Push Swap returned ${Color_Off}${Red}Error${Color_Off}"
+					elif [ $output -lt 701 ] ; then
+						echo -e "${Green}success -> $output ${Color_Off}"
+					else
+						echo -e "${Red}Error in instructions the number is : ${Color_Off}" $output
+					fi
+					((loop++))
+				done
+			else
+				echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
+			fi
+
+		elif [ $response -eq 5 ] ; then
+			if [ -x "$executable" ] ; then
+				loop=0
+				while [ $loop -le 200 ];
+				do
+					input=$(jot -r 5 -10000000 10000000)
+					output=$(./$executable $input | wc -l)
+					out=$(./$executable $input)
+					if [[ $out =~ ^[0-9]+$ ]]; then
+						echo -e "${Red}Error: ${Color_Off}push_swap returned a malformed return"
+					fi
+					if [[ $out = "Error" ]] ; then
+						echo -e "${Purple}Your Push Swap returned ${Color_Off}${Red}Error${Color_Off}"
+					elif [ $output -lt 13 ] ; then
+						echo -e "${Green}success -> $output ${Color_Off}"
+					else
+						echo -e "${Red}Error in instructions the number is : ${Color_Off}" $output
+					fi
+					((loop++))
+				done
+			else
+				echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
+			fi
+
+		elif [ $response -eq 3 ] ; then
+			if [ -x "$executable" ] ; then
+				loop=0
+				while [ $loop -le 200 ];
+				do
+					input=$(jot -r 3 -10000000 10000000)
+					output=$(./$executable $input | wc -l)
+					out=$(./$executable $input)
+					if [[ $out =~ ^[0-9]+$ ]]; then
+						echo -e "${Red}Error: ${Color_Off}push_swap returned a malformed return"
+					fi
+					if [[ $out = "Error" ]] ; then
+						echo -e "${Purple}Your Push Swap returned ${Color_Off}${Red}Error${Color_Off}"
+					elif [ $output -lt 3 ] ; then
+						echo -e "${Green}success -> $output ${Color_Off}"
+					else
+						echo -e "${Red}Error in instructions the number is : ${Color_Off}" $output
+					fi
+					((loop++))
+				done
+			else
+				echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
+			fi
+
 		else
-			echo -e "${Green}Success ${Color_Off}"
+			echo -e "${Red}Invalid Option${Color_Off}"
+
 		fi
-		((counter++))
 	done
-
-while true ; do
-	echo -e "options${Blue} : 3 5 100 500"
-	echo -e "${Cyan}Whats your desire number to test : ${Color_Off}"
-	read response
-
-	if [ $response -eq 500 ] ; then
-		if [ -x "$executable" ] ; then
-			loop=0
-			while [ $loop -le 200 ];
-			do
-				input=$(jot -r 500 -10000000 10000000)
-				output=$(./$executable $input | wc -l)
-				if [ $output -lt 5501 ] ; then
-					echo -e "${Green}success -> $output ${Color_Off}"
-				else
-					echo -e "${Red}Error in instructions the number is : ${Color_Off}" $output
-				fi
-				((loop++))
-			done
-		else
-			echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
-		fi
-
-	elif [ $response -eq 100 ] ; then
-		if [ -x "$executable" ] ; then
-			loop=0
-			while [ $loop -le 200 ]; 
-			do
-				input=$(jot -r 100 -10000000 10000000)
-				output=$(./$executable $input | wc -l)
-				if [ $output -lt 701 ] ; then
-					echo -e "${Green}success -> $output ${Color_Off}"
-				else
-					echo -e "${Red}Error in instructions the number is : ${Color_Off}" $output
-				fi
-				((loop++))
-			done
-		else
-			echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
-		fi
-
-	elif [ $response -eq 5 ] ; then
-		if [ -x "$executable" ] ; then
-			loop=0
-			while [ $loop -le 200 ];
-			do
-				input=$(jot -r 5 -10000000 10000000)
-				output=$(./$executable $input | wc -l)
-				if [ $output -lt 13 ] ; then
-					echo -e "${Green}success -> $output ${Color_Off}"
-				else
-					echo -e "${Red}Error in instructions the number is : ${Color_Off}" $output
-				fi
-				((loop++))
-			done
-		else
-			echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
-		fi
-
-	elif [ $response -eq 3 ] ; then
-		if [ -x "$executable" ] ; then
-			loop=0
-			while [ $loop -le 200 ];
-			do
-				input=$(jot -r 3 -10000000 10000000)
-				output=$(./$executable $input | wc -l)
-				if [ $output -lt 3 ] ; then
-					echo -e "${Green}success -> $output ${Color_Off}"
-				else
-					echo -e "${Red}Error in instructions the number is : ${Color_Off}" $output
-				fi
-				((loop++))
-			done
-		else
-			echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
-		fi
-
-	else
-		echo -e "${Red}Invalid Option${Color_Off}"
-
-	fi
-done
+else
+	echo -e "${Red}executable not found ${Color_Off}must be written as follow ${Green}$executable ${Color_Off}"
+fi
